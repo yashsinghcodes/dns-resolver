6764c2fb7787ced8b7bfd02c747b50d8af4cfd05
yashsinghcodes <yash9vardhan@gmail.com>
2023-05-17 21:21:39 +0530

    response parser

---
+++ pkg/parser/parser.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"net"
+	"os"
+
+	q "github.com/yashsinghcodes/dns-resolver/pkg/query"
+)
+
+type DNSRecord struct {
+	name   []byte
+	type_  int
+	class_ int
+	ttl_   int
+	data   []byte
+}
+
+func ParseHeader(buf *bytes.Buffer) q.DNSHeader {
+	var data q.DNSHeader
+	binary.Read(buf, binary.BigEndian, &data)
+	return data
+}
+
+func DecodeName(buf *bytes.Buffer) []byte {
+	var name [][]byte
+	for {
+		lengthByte := make([]byte, 1)
+		_, err := buf.Read(lengthByte)
+		if err != nil {
+			panic("An Error caused in reading")
+		}
+		length := int(lengthByte[0])
+		if length == 0 {
+			break
+		}
+		part := make([]byte, length)
+		buf.Read(part)
+		name = append(name, part)
+	}
+	return bytes.Join(name, []byte("."))
+}
+
+func Testresp() []byte {
+	queryy := q.Build_query("www.example.com", 1, 1, 1)
+	addr, err := net.ResolveUDPAddr("udp", "8.8.8.8:53")
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "Error in connecting")
+		return nil
+	}
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "Error in connecting")
+		return nil
+	}
+	_, err = conn.Write(queryy)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "Error in connecting")
+		return nil
+	}
+	res := make([]byte, 1024)
+	n, _, err := conn.ReadFromUDP(res)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "Error in connecting")
+		return nil
+	}
+	return res[:n]
+}
+
+func main() {
+	// sample response for now
+	myBytes := Testresp()
+	buf := bytes.NewBuffer(myBytes)
+	findata := ParseHeader(buf)
+	domainName := DecodeName(buf)
+	fmt.Println(findata, domainName)
+	fmt.Printf("%s\n", domainName)
+}
